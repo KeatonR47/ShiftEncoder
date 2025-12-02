fa3057a1220a5dd4dc8cfc0b7988d2475d939bc1
unknown <keaton.rosquist@engineering.snow>
2025-12-02 15:38:49 -0700

    Project original state

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Spencer & Keaton Lab 01 8/26/2025
+
+
+
+System.Console.WriteLine("automated testing ...");
+
+System.Diagnostics.Debug.Assert(ShiftedCharacter('a') == 'b');
+
+System.Diagnostics.Debug.Assert(ShiftedCharacter('z') == 'a');
+
+System.Console.WriteLine("tests passed.");
+
+
+
+
+char AsChar(int asciiValue) {
+
+    return (char) asciiValue;
+
+}
+
+
+
+char ShiftedCharacter(char originalCharacter) {
+
+    return AsChar((originalCharacter) == 'z' || originalCharacter == 'Z' ? (originalCharacter - 25) : (originalCharacter + 1));
+
+}
+
+
+
+Console.Write("Please Enter a character (I will display the shifted character): ");
+
+Console.WriteLine(ShiftedCharacter(Console.ReadKey(true).KeyChar));
